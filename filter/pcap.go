368a7669cfcb7d132c31770d185c120da38abadf
Alessandro Ghedini <alessandro@ghedini.me>
2014-12-17 22:13:11 +0100

    filter: improve API
    Make the Filter type be the BPF program itself. rename the Close() method to
    Cleanup() and add some documentation.

--- filter/pcap.go
+++ filter/pcap.go
@@ -1,3 +1,4 @@
+// Provides an API for compiling and manipulating BPF filters.
 package filter
 
 // #cgo LDFLAGS: -lpcap
@@ -10,10 +11,9 @@ import "unsafe"
 
 import "github.com/ghedo/hype/packet"
 
-type Filter struct {
-	bpf C.struct_bpf_program
-}
+type Filter C.struct_bpf_program
 
+// Compile the given tcpdump-like expression to a BPF filter.
 func Compile(filter string, link_type packet.Type) (*Filter, error) {
 	f := &Filter{}
 
@@ -26,7 +26,8 @@ func Compile(filter string, link_type packet.Type) (*Filter, error) {
 	pcap_type := link_type.ToLinkType()
 
 	err := C.pcap_compile_nopcap(
-		C.int(0x7fff), C.int(pcap_type), &f.bpf,
+		C.int(0x7fff), C.int(pcap_type),
+		(*C.struct_bpf_program)(f),
 		fil_str, 0, 0xffffffff,
 	)
 	if err < 0 {
@@ -36,6 +37,7 @@ func Compile(filter string, link_type packet.Type) (*Filter, error) {
 	return f, nil
 }
 
+// Try to match the given buffer against the filter.
 func (f *Filter) Match(raw_pkt []byte) bool {
 	var hdr C.struct_pcap_pkthdr
 
@@ -45,9 +47,11 @@ func (f *Filter) Match(raw_pkt []byte) bool {
 	hdr.len        = C.bpf_u_int32(hdr.caplen)
 	data          := (*C.u_char)(unsafe.Pointer(&raw_pkt[0]))
 
-	return C.pcap_offline_filter(&f.bpf, &hdr, data) != 0
+	bpf := (*C.struct_bpf_program)(f)
+	return C.pcap_offline_filter(bpf, &hdr, data) != 0
 }
 
-func (f *Filter) Close() {
-	C.pcap_freecode(&f.bpf)
+// Deallocate the filter.
+func (f *Filter) Cleanup() {
+	C.pcap_freecode((*C.struct_bpf_program)(f))
 }
