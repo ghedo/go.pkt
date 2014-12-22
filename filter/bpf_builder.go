da17d9ac173faf15434da732f7f2ed1a2c14f466
Alessandro Ghedini <alessandro@ghedini.me>
2014-12-22 19:01:03 +0100

    filter: improve descriptions of BPF instructions methods

--- filter/bpf_builder.go
+++ filter/bpf_builder.go
@@ -66,7 +66,9 @@ func (b *Builder) Build() *Filter {
 
 		/* if lbl, ok := b.jumps_k[i]; ok { */
 		/* 	addr := b.labels[lbl] */
-		/* 	insn.k = C.bpf_u_int32(addr - i - 1) */
+		/* 	if addr != 0 { */
+		/* 		insn.k = C.uint(addr - i - 1) */
+		/* 	} */
 		/* } */
 
 		if lbl, ok := b.jumps_jt[i]; ok {
@@ -94,96 +96,131 @@ func (b *Builder) Label(name string) *Builder {
 	return b
 }
 
-// Append a LD (load) instruction to the filter.
+// Append an LD instruction to the filter, which loads a value of size s into
+// the accumulator. m represents the addressing mode of the source operand and
+// can be IMM (load a constant value), ABS (load packet data at the given fixed
+// offset), IND (load packet data at the given relative offset), LEN (load the
+// packet length or MEM (load a value from memory at the given offset).
 func (b *Builder) LD(s Size, m Mode, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(m)) | LD
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a LDX (load index) instruction to the filter.
+// Append a LDX (load index) instruction to the filter, which loads a value of
+// size s into the index register. m represents the addressing mode of the
+// source operand and can be IMM (load a constant value), LEN (load the packet
+// length, MEM (load a value from memory at the given offset) or MSH (load the
+// length of the IP header).
 func (b *Builder) LDX(s Size, m Mode, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(m) | LDX)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a ST (store) instruction to the filter.
-func (b *Builder) ST(val uint32) *Builder {
-	b.filter.append_insn(ST, 0, 0, val)
+// Append a ST (store) instruction to the filter, which stores the value of the
+// accumulator in memory at the given offset.
+func (b *Builder) ST(off uint32) *Builder {
+	b.filter.append_insn(ST, 0, 0, off)
 	return b
 }
 
-// Append a STX (store to index) instruction to the filter.
-func (b *Builder) STX(val uint32) *Builder {
-	b.filter.append_insn(STX, 0, 0, val)
+// Append a STX (store index) instruction to the filter, which stores the value
+// of the index register in memory at the given offset.
+func (b *Builder) STX(off uint32) *Builder {
+	b.filter.append_insn(STX, 0, 0, off)
 	return b
 }
 
-// Append a ADD instruction to the filter.
+// Append an ADD instruction to the filter, which adds a value to the
+// accumulator. s represents the source operand type and can be either Const
+// (which adds the supplied value) or Index (which adds the index register
+// value).
 func (b *Builder) ADD(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x00) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a SUB instruction to the filter.
+// Append a SUB instruction to the filter, which subtracts a value from the
+// accumulator. s represents the source operand type and can be either Const
+// (which subtracts the supplied value) or Index (which subtracts the index
+// register value).
 func (b *Builder) SUB(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x10) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a MUL instruction to the filter.
+// Append a MUL instruction to the filter, which multiplies a value to the
+// accumulator. s represents the source operand type and can be either Const
+// (which multiplies the supplied value) or Index (which multiplies the index
+// register value).
 func (b *Builder) MUL(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x20) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a DIV instruction to the filter.
+// Append a DIV instruction to the filter, which divides the accumulator by a
+// value. s represents the source operand type and can be either Const (which
+// divides by the supplied value) or Index (which divides by the index register
+// value).
 func (b *Builder) DIV(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x30) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a AND instruction to the filter.
+// Append an AND instruction to the filter, which performs the binary "and"
+// between the accumulator and a value. s represents the source operand type and
+// can be either Const (which uses the supplied value) or Index (which uses the
+// index register value).
 func (b *Builder) AND(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x40) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a OR instruction to the filter.
+// Append an OR instruction to the filter, which performs the binary "or"
+// between the accumulator and a value. s represents the source operand type and
+// can be either Const (which uses the supplied value) or Index (which uses the
+// index register value).
 func (b *Builder) OR(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x50) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a LSH instruction to the filter.
+// Append an LSH instruction to the filter, which shifts to the left the
+// accumulator register by a value. s represents the source operand type and can
+// be either Const (which shifts by the supplied value) or Index (which shifts
+// by the index register value).
 func (b *Builder) LSH(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x60) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a RSH instruction to the filter.
+// Append an RSH instruction to the filter, which shifts to the right the
+// accumulator register by a value. s represents the source operand type and can
+// be either Const (which shifts by the supplied value) or Index (which shifts
+// by the index register value).
 func (b *Builder) RSH(s Src, val uint32) *Builder {
 	code := Code(uint16(s) | uint16(0x70) | ALU)
 	b.filter.append_insn(code, 0, 0, val)
 	return b
 }
 
-// Append a NEG instruction to the filter.
+// Append a NEG instruction to the filter which negates the accumulator.
 func (b *Builder) NEG() *Builder {
 	code := Code(uint16(0x80) | ALU)
 	b.filter.append_insn(code, 0, 0, 0)
 	return b
 }
 
-// Append a JA (jump absolute) instruction to the filter.
+// Append a JA instruction to the filter, which performs a jump to the given
+// label.
 func (b *Builder) JA(j string) *Builder {
 	b.jumps_k[b.filter.Len()] = j
 
@@ -192,7 +229,9 @@ func (b *Builder) JA(j string) *Builder {
 	return b
 }
 
-// Append a JEQ (jump if equal) instruction to the filter.
+// Append a JEQ instruction to the filter, which performs a jump to the jt label
+// if the accumulator value equals cmp (if s is Const) or the index register (if
+// s is Index), otherwise jumps to jf.
 func (b *Builder) JEQ(s Src, jt, jf string, cmp uint32) *Builder {
 	b.jumps_jt[b.filter.Len()] = jt
 	b.jumps_jf[b.filter.Len()] = jf
@@ -202,7 +241,9 @@ func (b *Builder) JEQ(s Src, jt, jf string, cmp uint32) *Builder {
 	return b
 }
 
-// Append a JGT (jump if greater than) instruction to the filter.
+// Append a JGT instruction to the filter, which performs a jump to the jt label
+// if the accumulator value is greater than cmp (if s is Const) or the index
+// register (if s is Index), otherwise jumps to jf.
 func (b *Builder) JGT(s Src, jt, jf string, cmp uint32) *Builder {
 	b.jumps_jt[b.filter.Len()] = jt
 	b.jumps_jf[b.filter.Len()] = jf
@@ -212,7 +253,9 @@ func (b *Builder) JGT(s Src, jt, jf string, cmp uint32) *Builder {
 	return b
 }
 
-// Append a JGE (jump if greater or equal) instruction to the filter.
+// Append a JGE instruction to the filter, which performs a jump to the jt label
+// if the accumulator value is greater than or equals cmp (if s is Const) or the
+// index register (if s is Index), otherwise jumps to jf.
 func (b *Builder) JGE(s Src, jt, jf string, cmp uint32) *Builder {
 	b.jumps_jt[b.filter.Len()] = jt
 	b.jumps_jf[b.filter.Len()] = jf
@@ -232,21 +275,26 @@ func (b *Builder) JSET(s Src, jt, jf string, cmp uint32) *Builder {
 	return b
 }
 
-// Append a RET (return) instruction to the filter.
+// Append a RET instruction to the filter, which terminates the filter program
+// and specifies the amount of the packet to accept. s represents the source
+// operand type and can be either Const (which returns the supplied value) or
+// Acc (which returns the accumulator value).
 func (b *Builder) RET(s Src, bytes uint32) *Builder {
 	code := Code(uint16(s) | RET)
 	b.filter.append_insn(code, 0, 0, bytes)
 	return b
 }
 
-// Append a TAX instruction to the filter.
+// Append a TAX instruction to the filter. TAX transfers the accumulator value
+// into the index register.
 func (b *Builder) TAX() *Builder {
 	code := Code(uint16(0x00) | MISC)
 	b.filter.append_insn(code, 0, 0, 0)
 	return b
 }
 
-// Append a TXA instruction to the filter.
+// Append a TXA instruction to the filter. TXA transfers the index register
+// value into the accumulator.
 func (b *Builder) TXA() *Builder {
 	code := Code(uint16(0x80) | MISC)
 	b.filter.append_insn(code, 0, 0, 0)
