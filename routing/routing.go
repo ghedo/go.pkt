eff934af0e18c674b857b7a129bf7220ffa02c80
Alessandro Ghedini <alessandro@ghedini.me>
2015-08-03 17:45:47 +0200

    routing: return route of the same type as the destination argument
    That is if the destination is IPv4, return an IPv4 route.

--- routing/routing.go
+++ routing/routing.go
@@ -80,6 +80,9 @@ func (r route_slice) Less(i, j int) bool {
 // Return the route that matches the given destination address.
 func RouteTo(dst net.IP) (*Route, error) {
 	var def *Route
+	var is_ipv4 bool
+
+	is_ipv4 = dst.To4() != nil
 
 	routes, err := Routes()
 	if err != nil {
@@ -90,6 +93,7 @@ func RouteTo(dst net.IP) (*Route, error) {
 
 	for _, r := range routes {
 		if r.Default &&
+		   (is_ipv4 && r.Gateway.To4() != nil) &&
 		   r.Iface != nil &&
 		   r.Iface.Flags & net.FlagLoopback == 0 {
 			def = r
