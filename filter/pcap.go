06bc0d3a9555208bc22f3fc77ef6d5f0a1f95202
Alessandro Ghedini <alessandro@ghedini.me>
2015-11-10 14:09:24 +0000

    filter: use better variable name

--- filter/pcap.go
+++ filter/pcap.go
@@ -44,15 +44,15 @@ import "github.com/ghedo/go.pkt/packet"
 func Compile(filter string, link_type packet.Type) (*Filter, error) {
 	f := &Filter{}
 
-	fil_str := C.CString(filter)
-	defer C.free(unsafe.Pointer(fil_str))
+	filter_str := C.CString(filter)
+	defer C.free(unsafe.Pointer(filter_str))
 
 	pcap_type := link_type.ToLinkType()
 
 	err := C.pcap_compile_nopcap(
 		C.int(0x7fff), C.int(pcap_type),
 		(*C.struct_bpf_program)(f.Program()),
-		fil_str, 0, 0xffffffff,
+		filter_str, 0, 0xffffffff,
 	)
 	if err < 0 {
 		return nil, fmt.Errorf("Could not compile filter")
