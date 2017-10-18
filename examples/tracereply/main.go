4396e2532c10d4774d14bbd451f809e918191ffb
Alessandro Ghedini <alessandro@ghedini.me>
2017-10-18 07:09:30 +0100

    examples: clean-up tracereply

--- examples/tracereply/main.go
+++ examples/tracereply/main.go
@@ -33,12 +33,13 @@ Reply to ICMPv6 traceroutes.`
         log.Fatalf("Error parsing hop paramenter: %s", err)
     }
 
-    capture, err := water.NewTUN(netif)
+    config := water.Config{ DeviceType: water.TUN }
+    config.Name = netif
+
+    capture, err := water.New(config)
     if err != nil {
         log.Fatalf("Error creating capture interface: %s", err)
     }
-
-    log.Println(ip)
 
     for {
         buf := make([]byte, 1500)
@@ -61,12 +62,8 @@ Reply to ICMPv6 traceroutes.`
             continue;
         }
 
-        if ip_pkt.Payload() == nil {
-            continue;
-        }
-
         icmp_pkt := layers.FindLayer(pkt, packet.ICMPv6)
-        if ip_pkt == nil {
+        if icmp_pkt == nil {
             continue;
         }
 
@@ -81,6 +78,8 @@ Reply to ICMPv6 traceroutes.`
 
         ttl := ip_pkt.(*ipv6.Packet).HopLimit
 
+        reply_pkts := []packet.Packet{ reply_ip_pkt, reply_icmp_pkt }
+
         switch {
         case uint64(ttl) < hops:
             []byte(ip)[len(ip) - 1] = ttl
@@ -90,14 +89,7 @@ Reply to ICMPv6 traceroutes.`
             reply_icmp_pkt.Type = icmpv6.TimeExceeded
             reply_icmp_pkt.Code = 0
 
-            reply_buf, err := layers.Pack(reply_ip_pkt, reply_icmp_pkt,
-                                                ip_pkt,       icmp_pkt)
-            if err != nil {
-                log.Printf("Error while packing: %s\n", err)
-                continue;
-            }
-
-            capture.Write(reply_buf)
+            reply_pkts = append(reply_pkts, ip_pkt, icmp_pkt)
 
         case uint64(ttl) >= hops:
             reply_ip_pkt.SrcAddr = ip_pkt.(*ipv6.Packet).DstAddr
@@ -105,14 +97,14 @@ Reply to ICMPv6 traceroutes.`
             reply_icmp_pkt.Type = icmpv6.EchoReply
             reply_icmp_pkt.Code = 0
             reply_icmp_pkt.Body = icmp_pkt.(*icmpv6.Packet).Body
-
-            reply_buf, err := layers.Pack(reply_ip_pkt, reply_icmp_pkt)
-            if err != nil {
-                log.Printf("Error while packing: %s\n", err)
-                continue;
-            }
-
-            capture.Write(reply_buf)
         }
+
+        reply_buf, err := layers.Pack(reply_pkts...)
+        if err != nil {
+            log.Printf("Error while packing: %s\n", err)
+            continue;
+        }
+
+        capture.Write(reply_buf)
     }
 }
